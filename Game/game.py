c003022425f57875961d236083f4644841152ec2
Filip <filipwor@gmail.com>
2023-06-04 22:20:27 +0200

    beer is appearing

--- Game/game.py
+++ Game/game.py
@@ -1,59 +1,136 @@
 import pygame
 from utils import load_sprite, get_random_position
-from models import Spaceship, Asteroid
+from models import Spaceship, Asteroid, Beer
+import pygame_menu
+
+BEER_RESPING_TIME = 5
 
 
 def _init_pygame():
+    # pygame init is responsible for loading all necessary modules
     pygame.init()
+    # window name
     pygame.display.set_caption("Spaceship game")
 
 
+def _menu():
+    _init_pygame()
+    surface = pygame.display.set_mode((1000, 667))
+    space_ship_game = SpaceShipGame(surface)
+    menu = pygame_menu.Menu('Welcome', 1000, 667, theme=pygame_menu.themes.THEME_BLUE)
+    menu.add.button('Play', space_ship_game.main_loop)
+    menu.add.button('Quit', quit)
+    menu.mainloop(surface)
+
+
 class SpaceShipGame:
-    def __init__(self):
-        _init_pygame()
-        self.screen = pygame.display.set_mode((1000, 667))
+    MIN_DISTANCE = 250
+
+    def __init__(self, surface):
+        self.screen = surface
         self.background = load_sprite("space.jpg", False)
         self.clock = pygame.time.Clock()
-        self.spaceship = Spaceship((400, 300))
-        self.asteroids = [Asteroid(get_random_position(self.screen)) for _ in range(6)]
+        self.bullets = []
+        self.spaceship = Spaceship((400, 300), self.bullets.append)
+        # self.asteroids = [Asteroid(get_random_position(self.screen)) for _ in range(6)]
+        self.asteroids = []
+        self.beer = []
+        self.time = time.time()
+
+        for _ in range(6):
+            while True:
+                position = get_random_position(self.screen)
+                if position.distance_to(self.spaceship.position) > self.MIN_DISTANCE:
+                    break
+            self.asteroids.append(Asteroid(position))
 
     def main_loop(self):
         while True:
             self.handle_input()
             self._process_game_logic()
             self._draw()
-            print("Prędkość: " + self.spaceship.velocity.__str__() + "Kierunek: " + self.spaceship.direction.__str__())
 
     def handle_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
+            elif (
+                    self.spaceship
+                    and event.type == pygame.KEYDOWN
+                    and event.key == pygame.K_SPACE
+            ):
+                self.spaceship.shoot()
 
         is_key_pressed = pygame.key.get_pressed()
 
-        if is_key_pressed[pygame.K_RIGHT]:
-            self.spaceship.rotate(clockwise=True)
-        elif is_key_pressed[pygame.K_LEFT]:
-            self.spaceship.rotate(clockwise=False)
-        if is_key_pressed[pygame.K_UP]:
-            self.spaceship.accelerate()
-        elif is_key_pressed[pygame.K_DOWN]:
-            self.spaceship.brake()
-        # else:
-        #     self.spaceship.slow()
-        if is_key_pressed[pygame.K_LCTRL]:
-            self.spaceship.stop()
+        if self.spaceship:
+            if is_key_pressed[pygame.K_RIGHT]:
+                self.spaceship.rotate(clockwise=True)
+            elif is_key_pressed[pygame.K_LEFT]:
+                self.spaceship.rotate(clockwise=False)
+            if is_key_pressed[pygame.K_UP]:
+                self.spaceship.accelerate()
+            elif is_key_pressed[pygame.K_DOWN]:
+                self.spaceship.brake()
+            else:
+                self.spaceship.slow()
+            if is_key_pressed[pygame.K_LCTRL]:
+                self.spaceship.stop()
 
     def _process_game_logic(self):
         for game_object in self._get_game_objects():
             game_object.move(self.screen)
 
+        if self.spaceship:
+            for asteroid in self.asteroids:
+                if asteroid.collides_with(self.spaceship):
+                    # fajnie by bylo dodac jakis wybuch po zderzeniu
+                    self.spaceship = None
+                    break
+
+        for bullet in self.bullets[:]:
+            for asteroid in self.asteroids[:]:
+                if asteroid.collides_with(bullet):
+                    self.asteroids.remove(asteroid)
+                    self.bullets.remove(bullet)
+                    break
+            if not self.screen.get_rect().collidepoint(bullet.position):
+                self.bullets.remove(bullet)
+
+        current_time = time.time()
+        if current_time - self.time > BEER_RESPING_TIME:
+            while True:
+                position = get_random_position(self.screen)
+                if position.distance_to(self.spaceship.position) > self.MIN_DISTANCE:
+                    break
+                self.time = time.time()
+            self.beer.append(Beer(position))
+        if self.spaceship:
+            self.spaceship.use_fuel()
+            for beer in self.beer[:]:
+                if self.spaceship.collides_with(beer):
+                    self.spaceship.add_fuel()
+                    self.beer.remove(beer)
+
+        # for bullet in self.bullets[:]:
+        #     if not self.screen.get_rect().collidepoint(bullet.position):
+        #         self.bullets.remove(bullet)
+
     def _draw(self):
+        # draw one image onto another
         self.screen.blit(self.background, (0, 0))
         for game_object in self._get_game_objects():
             game_object.draw(self.screen)
+        for beer in self.beer:
+            beer.draw(self.screen)
+        # updating display
         pygame.display.flip()
         self.clock.tick(60)
 
     def _get_game_objects(self):
-        return [*self.asteroids, self.spaceship]
+        game_objects = [*self.asteroids, *self.bullets]
+
+        if self.spaceship:
+            game_objects.append(self.spaceship)
+
+        return game_objects
--- Game/models.py
+++ Game/models.py
@@ -1,26 +1,22 @@
 from pygame.math import Vector2
-from utils import load_sprite, wrap_position
+from utils import load_sprite, get_random_velocity, load_sound
 from pygame.transform import rotozoom
-
+import threading
 UP = Vector2(0, -1)
 DOWN = Vector2(0, 1)
 
 
 class GameObject:
-    def __init__(self, position, sprite, velocity):
+    def __init__(self, position, sprite):
         self.position = Vector2(position)
         self.sprite = sprite
         self.radius = sprite.get_width() / 2
-        self.velocity = Vector2(velocity)
 
     def draw(self, surface):
         # Calculating blit position
         # It is needed because blit() method requires a top-left corner position
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
-
-    def move(self, surface):
-        self.position = wrap_position(self.position + self.velocity, surface)
 
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
@@ -29,25 +25,37 @@ class GameObject:
 
 class Spaceship(GameObject):
     MANEUVERABILITY = 3
-    ACCELERATION = 0.2
-    BRAKE = 0.1
+    ACCELERATION = 0.3
+    BRAKE = 0.25
     RESISTANCE = 0.05
+    MAX_SPEED = 10
+    BULLET_SPEED = 3
+    FUEL_CAPACITY = 10
 
-    def __init__(self, position):
+    def __init__(self, position, create_bullet_callback):
         self.direction = Vector2(UP)
-        super().__init__(position, load_sprite("ufo1_small.png"), Vector2(0))
+        self.create_bullet_callback = create_bullet_callback
+        self.laser_sound = load_sound("laser.mp3")
+        self.velocity = Vector2(0)
+        self.fuel = self.FUEL_CAPACITY
+        self.key_lock = threading.Lock()
+        super().__init__(position, load_sprite("ufo1_small.png"))
+
+    def move(self, surface):
+        if self.fuel != 0:
+            w, h = surface.get_size()
+            if 0 < self.position.x + self.velocity.x < w and 0 < self.position.y + self.velocity.y < h:
+                self.position = self.position + self.velocity
+            else:
+                self.velocity = Vector2(0)
 
     def accelerate(self):
-        if 10 > self.velocity.x > -10 and 10 > self.velocity.y > -10:
+        if 10 > (self.velocity + self.direction * self.ACCELERATION).length():
             self.velocity += self.direction * self.ACCELERATION
-        else:
-            self.velocity = self.direction.copy()
 
     def brake(self):
-        if not 0.1 > self.velocity.x > -0.1 or not 0.1 > self.velocity.y > -0.1:
-            self.velocity -= self.velocity * self.BRAKE
-        else:
-            self.velocity = Vector2(0)
+        if 10 > (self.velocity - self.direction * self.ACCELERATION).length():
+            self.velocity -= self.direction * self.BRAKE
 
     def stop(self):
         self.velocity = Vector2(0)
@@ -65,12 +73,66 @@ class Spaceship(GameObject):
         surface.blit(rotated_surface, blit_position)
 
     def slow(self):
-        if not 0.9 > self.velocity.y > -0.9 and not 0.9 > self.velocity.x > -0.9:
+        if self.MAX_SPEED >= self.velocity.length() > 0.01:
             self.velocity -= self.velocity * self.RESISTANCE
         else:
             self.velocity = Vector2(0)
 
+    def shoot(self):
+        bullet_velocity = self.direction * self.BULLET_SPEED + self.velocity
+        bullet = Bullet(self.position, bullet_velocity)
+        self.create_bullet_callback(bullet)
+        self.laser_sound.play()
+
+    def use_fuel(self):
+        self.key_lock.acquire()
+        if self.fuel - 0.001 * self.FUEL_CAPACITY > 0:
+            self.fuel -= 0.001 * self.FUEL_CAPACITY
+        else:
+            self.fuel = 0
+        self.key_lock.release()
+        print(self.fuel)
+
+    def add_fuel(self):
+        self.key_lock.acquire()
+        if self.fuel + 0.2 * self.FUEL_CAPACITY > self.FUEL_CAPACITY:
+            self.fuel = self.FUEL_CAPACITY
+        else:
+            self.fuel += 0.2 * self.FUEL_CAPACITY
+        self.key_lock.release()
+
 
 class Asteroid(GameObject):
     def __init__(self, position):
-        super().__init__(position, load_sprite("meteor1_smaller.png"), (0, 0))
+        self.velocity = get_random_velocity(1, 3)
+        super().__init__(position, load_sprite("asteroid_smaller.png"))
+
+    def move(self, surface):
+        w, h = surface.get_size()
+        if self.position.x > w or self.position.x < 0:
+            self.velocity.x = -self.velocity.x
+        if self.position.y > h or self.position.y < 0:
+            self.velocity.y = -self.velocity.y
+        self.position += self.velocity
+
+
+class Bullet(GameObject):
+    def __init__(self, position, velocity):
+        self.velocity = velocity
+        super().__init__(position, load_sprite("bullet_smaller.png"))
+
+    def draw(self, surface):
+        angle = self.velocity.angle_to(UP)
+        rotated_surface = rotozoom(self.sprite, angle, 1.0)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
+
+    def move(self, surface):
+        self.position += self.velocity
+
+
+class Beer(GameObject):
+    def __init__(self, position):
+        super().__init__(position, load_sprite("piwo_ale_takie_male.png"))
+
