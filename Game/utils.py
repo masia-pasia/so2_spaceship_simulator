cd0e61ab81a1060a6a98a3288c1b402f19ad2440
Filip <filipwor@gmail.com>
2023-06-04 20:03:07 +0200

    initial commit

--- Game/utils.py
+++ Game/utils.py
@@ -19,12 +19,6 @@ def load_sound(name):
     return Sound(path)
 
 
-def wrap_position(position, surface):
-    x, y = position
-    w, h = surface.get_size()
-    return Vector2(x % w, y % h)
-
-
 def get_random_position(surface):
     return Vector2(
         random.randrange(surface.get_width()),
